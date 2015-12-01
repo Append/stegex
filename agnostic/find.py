7bedc2369f57b3bb62b120a08d2dc5c1587c81cf
append <ghunter126@yahoo.com>
2015-11-30 19:23:09 -0600

    Updated find and search to handle args

--- agnostic/find.py
+++ agnostic/find.py
@@ -1,17 +1,34 @@
+import argparse
 
 def main():
+	#simple arg parse to get filenames
+	parser = argparse.ArgumentParser()
+	parser.add_argument("key", type=str,help="the keyfile")
+	parser.add_argument("output", type=str,help="where you are ouputing the secret")
+	parser.add_argument("payload", type=str,help="the payload used to store the data")
+	parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+	args = parser.parse_args()
+
+	#bytearray for storing the bytes we pull out of the file
 	newFileByteArray = bytearray()
-	key = open("key2.txt",'rb')
-	fsec = open('secret.jpeg','wb')
-	payload = open("C:\Windows\system32\calc.exe", 'rb')
+	#open the keyfile with relative locations of bytes
+	key = open(args.key,'rb')
+	#open file for outputing the stuff too later
+	fsec = open(args.output,'wb')
+	#open the file we used to hide the data in, must be the same version as the one that was used to encode the data
+	payload = open(args.payload, 'rb')
+	#parse the keyfile
 	keylist = key.read()
 	keylist = keylist.split(',')
 	keylist = keylist[:-1]
+	#walk through the key list
 	for loc in keylist:
+		#make filepointer to the byte that was in the key
 		payload.seek(int(loc),0)
+		#append that byte to the output byte array
 		newFileByteArray.append(payload.read(1))
 
-	#print newFileByteArray
+	#output bytearray to file
 	fsec.write(newFileByteArray)
 
 if __name__ == "__main__": main()
--- agnostic/search.py
+++ agnostic/search.py
@@ -1,4 +1,6 @@
+import argparse
 
+#this method reads in the file in binary to a buffer passed back to the main func, used for improved processing speed by buffering the object
 def bs_from_file(filename, chunksize=8192):
     with open(filename, "rb") as f:
         while True:
@@ -10,11 +12,11 @@ def bs_from_file(filename, chunksize=8192):
                 break
     f.close()
 
-#this function contains the find call
+#this function contains the find call/ find() uses the Boyer-moore-horspool algorithm
 def search(buff, cut_s):
 	return buff.find(cut_s)
 
-#search for string
+#search for string/made to handle a multi byte input, however the current code version only sends 1 byte at a time
 def cut(buff, minus_s):
 	#full length of string then repeat with len-1 til found then return a tuple of the location and how many bs
 	while(len(minus_s)>0):
@@ -27,15 +29,25 @@ def cut(buff, minus_s):
 			#print minus_s
 
 def main():
-	# example:
+	#simple arg parser
+	parser = argparse.ArgumentParser()
+	parser.add_argument("secret", type=str,help="the file you are attempting to hid")
+	parser.add_argument("output", type=str,help="where you are ouputing the key")
+	parser.add_argument("payload", type=str,help="the payload")
+	parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+	args = parser.parse_args()
+	
 	output = []
-	for b in bs_from_file('test.jpeg'):
-		# Do stuff with b.
-		with open("C:\Windows\system32\calc.exe", 'rb') as f:
+	#open secret with the first method
+	for b in bs_from_file(args.secret):
+		#open payload and read into buffer
+		with open(args.payload, 'rb') as f:
 			buff = f.read()
+			#run search against the byte and store it
 			output.append(cut(buff,b)[0])
 
-	fout = open("key2.txt",'wb')
+	#output all the bytes locations to the keyfile
+	fout = open(args.output,'wb')
 
 	for thing in output:
 		fout.write(str(thing))
