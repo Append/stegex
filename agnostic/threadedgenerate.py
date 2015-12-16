cc37a936328d042cf9651a1a45a4db9b56778c7d
append <ghunter126@yahoo.com>
2015-12-15 21:40:16 -0600

    created a new threaded version

--- agnostic/threadedgenerate.py
+++ agnostic/threadedgenerate.py
@@ -1,4 +1,10 @@
 import argparse
+import os
+# drop in replacement for threading when we want a return value
+from multiprocessing.pool import ThreadPool
+import threading
+
+output = {}
 
 # credit for this "bs_from_file" method to http://stackoverflow.com/questions/1035340/reading-binary-file-in-python-and-looping-over-each-byte
 # by skurmedel
@@ -28,14 +34,14 @@ def search(buff, cut_s):
 # code version only sends 1 byte at a time
 
 
-def cut(buff, minus_s):
+def cut(buff, minus_s, idn):
     # full length of string then repeat with len-1 til found then return a
     # tuple of the location and how many bs
     while(len(minus_s) > 0):
         tool = search(buff, minus_s)
         if(tool != -1):
-            return (tool, minus_s)
-            print '1'
+            output[idn] = tool;
+            return
         else:
             minus_s = minus_s[:-1]
             # print minus_s
@@ -54,24 +60,39 @@ def main():
                         help="increase output verbosity", action="store_true")
     args = parser.parse_args()
 
-    output = []
+    #dictionary for output
+    threads = []
+    idn = 0
     # open secret with the first method
     for b in bs_from_file(args.secret):
+        idn += 1
         # open payload and read into buffer
         with open(args.payload, 'rb') as f:
+            # thread the searching for efficiency
             buff = f.read()
+
+            #while(threading.active_count()>700):
+            #    continue
             # run search against the byte and store it
-            key = cut(buff,b)
-            output.append(key[0])
+            # thread this but pass it a sequence id
+            #pool = ThreadPool(processes=1)
+            #async_result = pool.apply_async(cut, (buff, b, idn,))
+            t = threading.Thread(target=cut, args=(buff,b,idn,))
+            threads.append(t)
+            t.start()
+            #foo = async_result.get(timeout=2)
+            #output[foo] = idn;
+            # key = cut(buff,b)
+            # output.append(idn)
+            # output.append(key[0])
 
-    #run a sort on the output to order them by sequencer and then strip the id off of them
-
-    # output all the bytes locations to the keyfile
     fout = open(args.output, 'wb')
-
-    for thing in output:
-        fout.write(str(thing))
+    # run a sort on the output to order them by sequencer and then strip the id off of them
+    # output all the bytes locations to the keyfile
+    for key in sorted(output):
+        fout.write(str(output[key]))
         fout.write(',')
+        #print "%s: %s" % (key, output[key])
 
     fout.close()
 
